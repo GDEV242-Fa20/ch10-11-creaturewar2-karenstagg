976880f499ff8c7403015ef763ea37ddcd241e97
karenstagg <42982417+karenstagg@users.noreply.github.com>
2020-10-31 17:54:01 -0400

    Modify Creature Class
    complete the given methods, and add damage() and getHealth() methods.

--- Creature.java
+++ Creature.java
@@ -7,22 +7,23 @@
  * the creature is alive or knocked out. The creature is also responsible for calculating
  * damage delivered based on the creature's strength (1 to str) 
  * 
- * @author Crosbie
- * @version 2020-10 v1.0
+ * @author Karen Stagg
+ * @version November 09, 2020
  */
 // we will learn what the abstract keyword does in a later chapter
 public abstract class Creature
 {
     private int str;        // The strength of this creature
     private int max_hp;     // The maximum hit points the creature can have (used if healing enabled)
-    private int hp;         // the current numberof hit points the creature has
+    private int hp;         // the current number of hit points the creature has
     
     /**
      * default constructor - this should never actually run.
      * It is the job of dervived classes to set up the proper number of hit points and 
      * strength for the subclass
      */
-    public Creature (){
+    public Creature ()
+    {
         str=10;
         hp=10;
         max_hp = hp;
@@ -34,49 +35,83 @@ public abstract class Creature
      * Heals must never allow for more hit points than the creature started
      * with
      * @param str the strength of the creature, used to calculate damage
-     * @param hp the health of the creature at the start of the simulation, and the current health levels during battle
+     * @param hp the health of the creature at the start of the simulation, 
+     * and the current health levels during battle
      */
-    public Creature (int str, int hp) {
-       //implement this
+    public Creature (int str, int hp) 
+    {
+       this.str = str;
+       this.hp = hp;
+       max_hp  = hp;
     }
-    
     
     /**
      * Allows a creature to determine how much damage it is causing in this round of battle
      * @return a value between 1 and str to be used to cause damage to another creature
      */
-    public int attack(){
-        // TODO: implement a damage method
-        return 0;
+    public int attack()
+    {
+        return damage();
     }
     
-    
+    /**
+     * Determines how much damage a creature is causing in this round of battle
+     * @return a value between 1 and str to be used to cause damage to another creature
+     */
+    public int damage()
+    {
+        return Randomizer.nextInt(str);
+    }
+        
     /**
      * Is this creature still capable of fighting?
      * @return true when current hit point level is greater than zero
      */
-    public boolean isAlive() {
-        // TODO: implement a method to report if the creature yet lives
-        return false; //change this
+    public boolean isAlive() 
+    {
+        if (hp > 0)
+        {
+            return true; 
+        }
+        else
+        {
+            return false;
+        }    
     }
     
     /**
      * Is this creature knockedOut?
      * @return true when current hit point level is less than or equal to zero
      */
-    public boolean isKnockedOut() {
-        //TODO: implement a method to report if the creature has been killed
-        return false; //change this
+    public boolean isKnockedOut() 
+    {
+        if (hp <= 0)
+        {
+            return true; 
+        }
+        else
+        {
+            return false;
+        }    
     }
-    
-    
+        
     /**
      * takeDamage receives a value for the amount of damage to subtract from 
      * the current total of hit points
      * @param damage value to remove from hit point count
      */
-    public void takeDamage(int damage) {
-        // TODO: implement this
+    public void takeDamage(int damage) 
+    {
+        hp -= damage;
     }
     
+    /**
+     * getHealth returns the number of current hitpoints for the creature 
+     * 
+     * @return the number of current hitpoints the creature has
+     */
+    public int getHealth() 
+    {
+        return hp;
+    }
 }
